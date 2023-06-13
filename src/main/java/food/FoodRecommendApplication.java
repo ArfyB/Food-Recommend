1e75d54ba1b63198c0728e051bdf2fb58ab25296
ArfyB <arfybundle@gmail.com>
2023-06-14 05:57:44 +0900

    기본설정

---
+++ src/main/java/food/FoodRecommendApplication.java
@@ -0,0 +1,13 @@
+package food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRecommendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodRecommendApplication.class, args);
+	}
+
+}
